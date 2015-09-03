8582152eb1ff142fc919b0578728ae8e05d944b3
zatenzu <zatenzu@hotmail.com>
2015-09-03 18:18:24 +0200

    + initial import

---
+++ src/be/zatenzu/oca/js8/_class/Main.java
@@ -0,0 +1,16 @@
+package be.zatenzu.oca.js8._class;
+
+/**
+ * Created by tda on 3/09/2015.
+ */
+public class Main{
+  /**
+   *
+   * Only one public class per file
+   * The file name must match with the public class name
+   * A file can content more than 1 non-public class
+   * A file with no public class can have a name different from class name
+   * Only public default, abstract and final!
+   *
+   */
+}
