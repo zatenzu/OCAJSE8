26c77fdc5fe350be4c07b43459363b4fdeb25788
zatenzu <zatenzu@hotmail.com>
2015-09-14 08:24:48 +0200

    + non-wilrdcard import takes over wildcard import

---
+++ src/be/zatenzu/oca/js8/iimport/packageA/A.java
@@ -0,0 +1,7 @@
+package be.zatenzu.oca.js8.iimport.packageA;
+
+/**
+ * Created by tda on 13/09/2015.
+ */
+public class A{
+}
---
+++ src/be/zatenzu/oca/js8/iimport/packageB/A.java
@@ -0,0 +1,7 @@
+package be.zatenzu.oca.js8.iimport.packageB;
+
+/**
+ * Created by tda on 13/09/2015.
+ */
+public class A{
+}
