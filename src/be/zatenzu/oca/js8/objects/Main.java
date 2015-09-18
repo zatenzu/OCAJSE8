d3519590d583d1784c3b63a7d782d3d0eb80392e
zatenzu <zatenzu@hotmail.com>
2015-09-18 17:01:52 +0200

    + Object reference

--- src/be/zatenzu/oca/js8/objects/Main.java
+++ src/be/zatenzu/oca/js8/objects/Main.java
@@ -5,8 +5,23 @@ package be.zatenzu.oca.js8.objects;
  */
 public class Main{
 
-  //Objects are stored in the heap
-  //Object references are stored on the stack
+  public static void main(String[] args){
+
+    //Objects are stored in the heap
+    //Object references are stored on the stack
+
+    /** == **/
+    String s = "1";
+    StringBuilder sb = new StringBuilder("1");
+//  if(s == sb){}// Cannot compare reference of two different object types
+    NumberFormatException numberFormatException = new NumberFormatException();
+    IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
+    if(numberFormatException == illegalArgumentException){}//allowed because NFE is-a IAE
+    //Wrappers
+    Integer i = new Integer(0);
+    Integer ii = new Integer(0);
+    System.out.println(ii == i);//true because wrappers compare its primitive value
+  }
 
   /** finalize */
   @Override
