ca5add63722842aecb6b17a4b918469402db65af
zatenzu <zatenzu@hotmail.com>
2015-09-20 13:19:40 +0200

    + private via new instance

--- src/be/zatenzu/oca/js8/access/privateaccess/Main.java
+++ src/be/zatenzu/oca/js8/access/privateaccess/Main.java
@@ -6,4 +6,11 @@ package be.zatenzu.oca.js8.access.privateaccess;
 public class Main{
   //must be in the same class
   //inner class can access to private members
+
+  private int i = 0;
+
+  public static void main(String[] args){
+    Main m = new Main();
+    System.out.println(m.i);//ok I'm in the same class
+  }
 }
