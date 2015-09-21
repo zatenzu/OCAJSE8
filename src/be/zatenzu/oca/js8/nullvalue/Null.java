0a5510452adfef74bbf3aed7a18a40c5ae421663
zatenzu <zatenzu@hotmail.com>
2015-09-21 08:16:45 +0200

    + Initial import

---
+++ src/be/zatenzu/oca/js8/nullvalue/Null.java
@@ -0,0 +1,28 @@
+package be.zatenzu.oca.js8.nullvalue;
+
+public class Null{
+  /**
+   * - A null can be cast to anything without causing a NullPointerException
+   * - Unboxing can cause a NullPointerException
+   * - If null is passed to equals, false should be returned
+   * - If several methods with identical name but with different parameter type, casting null is required to
+   * chose which method will be call, excepted if parameter type have inheritance link. See below for examples
+   *
+   */
+
+  public static void main(String[] args){
+//    testNull(null);//casting null is required
+    testNull2(null);//String extends Object, the subtype is chosen. No casting required
+    testNull3(null);//Warning! Don't apply same rules as overloading.
+    // Integer... is var args but it's extends Object. So var args is chosen.
+  }
+
+  static void testNull(String s){}
+  static void testNull(StringBuilder builder){}
+
+  static void testNull2(Object s){}
+  static void testNull2(String o){}
+
+  static void testNull3(Object s){}
+  static void testNull3(Integer... o){}
+}
