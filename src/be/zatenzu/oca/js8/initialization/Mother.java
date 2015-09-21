82bc787cd07186eb937143aaac17cb8a8f6363e4
zatenzu <zatenzu@hotmail.com>
2015-09-21 08:15:11 +0200

    + anonymous bloc

--- src/be/zatenzu/oca/js8/initialization/Mother.java
+++ src/be/zatenzu/oca/js8/initialization/Mother.java
@@ -6,6 +6,8 @@ package be.zatenzu.oca.js8.initialization;
 public class Mother{
   static{System.out.println("MotherStaticBlock");}
 
+  {System.out.println("this bloc is never called if no instance is created");}
+
   public Mother(){
     System.out.println("Mother");
   }
