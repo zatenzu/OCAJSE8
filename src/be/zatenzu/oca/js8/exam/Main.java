3711af06ec7778809d7454ba4e42343e2c6d9f82
zatenzu <zatenzu@hotmail.com>
2015-09-22 08:50:33 +0200

    + required - allowed

--- src/be/zatenzu/oca/js8/exam/Main.java
+++ src/be/zatenzu/oca/js8/exam/Main.java
@@ -9,6 +9,7 @@ public class Main{
   //all other answers are true because the code break only at the line where the exception is thrown
   //and the code can show something before the exception.
   // - When the question is: "are they equal?" equal means equals() not ==
+  // - If something is required, it's allowed too.
 
   /** Vocabulary **/
   //- lenient: indulgent
