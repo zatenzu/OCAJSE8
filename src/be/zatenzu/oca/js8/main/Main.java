0195ff2568cda21b59552902af8777b49190f707
zatenzu <zatenzu@hotmail.com>
2015-09-28 16:58:32 +0200

    + Main

--- src/be/zatenzu/oca/js8/main/Main.java
+++ src/be/zatenzu/oca/js8/main/Main.java
@@ -9,11 +9,11 @@ class Main{
 //  public static void main(String abbalblalba[]){System.out.println("Hello world from non public class");}
 //  public static void main(String abbalblalba[]) throws Exception {System.out.println("Hello world from non public class");}
 
-
   /* illegal main declarations */
 //  static void main(String[] args){}//must be public
 //  public void main(String[] args){}//must be static
 //  public static void main(String agrs){}//must take a String array parameter (or varargs)
 //  public static void main(String[] agrs, int i){}//must take only a String array parameter (or varargs)
 //  public static int main(String... args){return 0;}//must return void
+//  public static void Main(String[] args){System.out.println("Hello world from non public class");} -> NoSuchMethodException
 }
