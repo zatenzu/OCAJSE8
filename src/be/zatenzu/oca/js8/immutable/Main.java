9594a6950686d46949543a7ace1f625ffafb6d8f
zatenzu <zatenzu@hotmail.com>
2015-09-28 16:56:48 +0200

    + cannot be modified and collected

--- src/be/zatenzu/oca/js8/immutable/Main.java
+++ src/be/zatenzu/oca/js8/immutable/Main.java
@@ -4,5 +4,10 @@ package be.zatenzu.oca.js8.immutable;
  * Created by tda on 15/08/2015.
  */
 public class Main{
+  /*************************************/
+  /** Immutability:                   **/
+  /** - Immutable object cannot be modified
+  /** - Immutable object can be garbage collected
+  /*************************************/
   //see page 207
 }
