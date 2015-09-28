990c827137ee60cfd52815a75c88b8addf788205
zatenzu <zatenzu@hotmail.com>
2015-09-28 17:08:28 +0200

    *

--- src/be/zatenzu/oca/js8/_abstract/a/_Abstract.java
+++ src/be/zatenzu/oca/js8/_abstract/a/_Abstract.java
@@ -53,13 +53,11 @@ public abstract class _Abstract
       public void myMethod()
       {
         // TODO Auto-generated method stub
-        
       }
       @Override
       public void myMethod2()
       {
         // TODO Auto-generated method stub
-        
       }
 
       @Override
