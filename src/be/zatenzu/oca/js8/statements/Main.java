6ea8b9b7f5af352df6dad46ad09dc0e9963a4788
zatenzu <zatenzu@hotmail.com>
2015-10-05 08:43:07 +0200

    *

--- src/be/zatenzu/oca/js8/statements/Main.java
+++ src/be/zatenzu/oca/js8/statements/Main.java
@@ -8,7 +8,7 @@ public class Main{
   public static void main(String[] args){
     /** ternary operator  */
     boolean b = true ? true : false;// boolean expression ? if the expression is true : if the expression is false
-  /* double ternary operator  */
+    /* double ternary operator  */
     boolean bb = true ? true ? true : false : false;
     //short circuit!
 
