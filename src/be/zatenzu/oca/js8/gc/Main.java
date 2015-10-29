16517d5c5803ff72c6e78f4ed6a83621bc846014
zatenzu <zatenzu@hotmail.com>
2015-10-29 10:14:10 +0100

    *

--- src/be/zatenzu/oca/js8/gc/Main.java
+++ src/be/zatenzu/oca/js8/gc/Main.java
@@ -7,7 +7,7 @@ public class Main{
 
   public static void main(String[] args){
     //call the garbage collector
-    //no garanteed to run!
+    //no guaranteed to run!
     System.gc();
 
     /** Eligible objects for gc are **/
