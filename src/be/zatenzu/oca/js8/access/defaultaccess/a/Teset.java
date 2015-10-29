f9b4f4c46d44405ce692c5485bb23445187b3b50
zatenzu <zatenzu@hotmail.com>
2015-10-29 10:10:41 +0100

    + test sub package

---
+++ src/be/zatenzu/oca/js8/access/defaultaccess/a/Teset.java
@@ -0,0 +1,12 @@
+package be.zatenzu.oca.js8.access.defaultaccess.a;
+
+import be.zatenzu.oca.js8.access.defaultaccess.Main;
+
+/**
+ * Created by tda on 24/10/2015.
+ */
+public class Teset{
+  public static void main(String[] args){
+//    System.out.println(new Main().x);Sub package isn't the same package
+  }
+}
