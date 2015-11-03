843f8f9e8655c6d7fa69469dfe7aa2dbcc6b28a1
zatenzu <zatenzu@hotmail.com>
2015-11-03 12:55:36 +0100

    + illegal forward reference

--- src/be/zatenzu/oca/js8/initialization/Main.java
+++ src/be/zatenzu/oca/js8/initialization/Main.java
@@ -13,7 +13,22 @@ public class Main{
    * 5) Constructors, super -> current
    */
   //if a static bloc, call constructor, keep rule 4 before 5! @see at page 204
+
+  {
+//    myVar ++;//illegal forward reference!
+    myVar = 1;
+//    myVar ++;//illegal forward reference!
+  }
+  int myVar = 2;
+  {myVar  =3;}
+  //you can init a var declared after the block but you cannot use its value
+  //before the initialization of it.
+
+  {myStaticVar++;}
+  static int myStaticVar = 0;
+
+
   public static void main(String[] args){
-    new Child();
+    System.out.println(new Main().myVar);//2!
   }
 }
