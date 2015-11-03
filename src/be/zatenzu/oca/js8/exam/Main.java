6f169d5c0409381c70080f04384b450c19593a61
zatenzu <zatenzu@hotmail.com>
2015-11-03 12:52:05 +0100

    + implicit and new abstract

--- src/be/zatenzu/oca/js8/exam/Main.java
+++ src/be/zatenzu/oca/js8/exam/Main.java
@@ -4,12 +4,14 @@ package be.zatenzu.oca.js8.exam;
  * Created by tda on 16/09/2015.
  */
 public class Main{
-  // - Warning to (Choose all that apply) !
+   // - Warning to (Choose all that apply) !
   // - If an exception is thrown, choose "An exception is thrown" answer but
   //all other answers are true because the code break only at the line where the exception is thrown
   //and the code can show something before the exception.
   // - When the question is: "are they equal?" equal means equals() not ==
   // - If something is required, it's allowed too.
+  // - Warning to implicit things, mainly interface (everything is public)
+  // - new and abstract 
 
   /** Vocabulary **/
   //- lenient: indulgent
