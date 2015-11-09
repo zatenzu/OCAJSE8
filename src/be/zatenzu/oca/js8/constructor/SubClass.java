b713ae17d799dd706f0885e30f2c2bc70d5cc265
zatenzu <zatenzu@hotmail.com>
2015-11-09 14:25:32 +0100

    *

--- src/be/zatenzu/oca/js8/constructor/SubClass.java
+++ src/be/zatenzu/oca/js8/constructor/SubClass.java
@@ -15,7 +15,7 @@ public class SubClass extends Mother
   //if you define a constructor, the implicit
   //default constructor disappears!
   
-  //compilator error because compilator want to insert super()
+  //compilation error because the compilator want to insert super()
   //at the first line of this constructor but the default no-args Mother
   //constructor doesn't exist and he cannot add it because there is already
   //a constructor in the Mother class.
