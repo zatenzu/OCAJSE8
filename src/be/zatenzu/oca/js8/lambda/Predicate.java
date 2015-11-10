a5412e22a9d47156f399a529fc7628b9e8a313a3
zatenzu <zatenzu@hotmail.com>
2015-11-10 10:02:16 +0100

    + predicate

---
+++ src/be/zatenzu/oca/js8/lambda/Predicate.java
@@ -0,0 +1,16 @@
+package be.zatenzu.oca.js8.lambda;
+
+/**
+ * Created by tda on 10/11/2015.
+ */
+public class Predicate{
+
+  /**
+   * Predicate:
+   *
+   * - java.util.function.Predicate ! Don't forget import!
+   * - Functional interface
+   * - public abstract boolean test(T t) T is Predicate<T>
+   *
+   */
+}
